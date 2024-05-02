524b0216d3135918fd1e1adcc4ec388871f2d543
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2024-05-03 01:03:10 +0300

    Added a slider for lines

--- src/components/ImageProblem.jsx
+++ src/components/ImageProblem.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react"
 import DeleteButton from "./DeleteButton"
 
 const ImageProblem = ({ number, lines, image, deleteProblem }) => {
+    const [lineAmount, setLineAmount] = useState(lines)
 
-    const linesToPrint = <p className="wrap">{"_".repeat(lines)}</p>
+   // const linesToPrint = 
 
     return (
         <div>
@@ -11,7 +13,8 @@ const ImageProblem = ({ number, lines, image, deleteProblem }) => {
             <div className="imageDisplay">
                 <img src={URL.createObjectURL(image)} width="100%" height="100%" />
             </div>
-            {linesToPrint}
+            <p contentEditable className="wrap">{"_".repeat(lineAmount)}</p>
+            <input style={{width:"40%"}} type="range" min={1} max={1000} value={lineAmount} onChange={e => setLineAmount(e.currentTarget.value)} />
         </div>
     )
 }
--- src/components/TextProblem.jsx
+++ src/components/TextProblem.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react"
 import DeleteButton from "./DeleteButton"
 
 const TextProblem = ({ number, lines, deleteProblem }) => {
-
-    const linesToPrint = <p className="wrap">{"_".repeat(lines)}</p>
+    const [lineAmount, setLineAmount] = useState(lines)
 
     return (
         <div>
             <h2 contentEditable suppressContentEditableWarning="true">Problem {number}</h2>
             <DeleteButton deleteProblem={deleteProblem} order={number} />
-            {linesToPrint}
+            
+            <p contentEditable className="wrap">{"_".repeat(lineAmount)}</p>
+            <input style={{width:"40%"}} type="range" min={1} max={1000} value={lineAmount} onChange={e => setLineAmount(e.currentTarget.value)} />
         </div>
     )
 }
